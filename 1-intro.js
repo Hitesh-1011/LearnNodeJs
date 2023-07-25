070a7104bbf8a8aa3cddaa3e0aa407a17274e068
Hitesh <78798453+Hitesh-1011@users.noreply.github.com>
2023-07-25 11:05:05 +0530

    Add files via upload

---
+++ 1-intro.js
@@ -0,0 +1,14 @@
+
+const a = 9;
+
+if (a > 10) {
+    console.log("large number");
+} else {
+    console.log("small number");
+}
+
+console.log("First Project running successfully");
+
+// About the file
+// calculations and all the console logs can be printed While Running this file
+// To run this file node 1-intro
